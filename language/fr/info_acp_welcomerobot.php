4c5aba81f2710580db4c92edb39c79531c8fbf86
Raphaël M. <Galixte@users.noreply.github.com>
2015-03-20 12:01:15 +0100

    FRENCH translation for Welcome Robot extension
    FRENCH translation for info_acp_welcomerobot.php file

---
+++ language/fr/info_acp_welcomerobot.php
@@ -0,0 +1,39 @@
+<?php
+/**
+*
+* Welcome Robot extension for the phpBB Forum Software package.
+* French translation by Galixte (http://www.galixte.com)
+*
+* @copyright eMosbat & (c) 2014 Ali Faraji(Ali@php) <alifaraji.mail@gmail.com> phpbbpersian.ir
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+$lang = array_merge($lang, array(
+	'ACP_WELCOMEROBOT_MOD_TITLE'					=> 'Sujet de bienvenue',
+	'ACP_WELCOMEROBOT_CONFIG_TITLE'					=> 'Sujet automatique de bienvenue',
+	'ACP_WELCOMEROBOT_TITLE'						=> 'Sujet automatique de bienvenue',
+	'ACP_WELCOMEROBOT_LEGEND1'						=> 'Paramètres',
+	'ACP_WELCOMEROBOT_CONFIG_ENABLE'				=> 'Activer',
+	'ACP_WELCOMEROBOT_CONFIG_FORUM'					=> 'Forum',
+	'ACP_WELCOMEROBOT_CONFIG_FORUM_EXPLAIN'			=> 'Créer les sujets dans ce forum.',
+	'ACP_WELCOMEROBOT_CONFIG_USERNAME'				=> 'Nom du robot',
+	'ACP_WELCOMEROBOT_CONFIG_USERNAME_EXPLAIN'		=> 'Nom qui sera attribué au robot, auteur des sujets.',
+	'ACP_WELCOMEROBOT_CONFIG_TOPICTITLE'			=> 'Titre des sujets',
+	'ACP_WELCOMEROBOT_CONFIG_TOPICTITLE_EXPLAIN'	=> 'Titre des sujets créés lors de l’enregistrement de nouveaux utilisateurs.',
+	'ACP_WELCOMEROBOT_CONFIG_DETAIL'				=> 'Contenu des sujets',
+	'ACP_WELCOMEROBOT_CONFIG_DETAIL_EXPLAIN'		=> 'Contenu des sujets créés lors de l’enregistrement de nouveaux utilisateurs.',
+	'ACP_WELCOMEROBOT_CONFIG_HELP'					=> 'Note :<br />vous pouvez utiliser `%user`, `%robot` et `%board` pour afficher dans le titre et le contenu du sujet, le nom du dernier utilisateur enregistré, le nom du robot et le titre du forum.',
+	'ACP_WELCOMEROBOT_SETTINGS'						=> 'Paramètres',
+	'ACP_WELCOMEROBOT_SUCCESS'						=> 'Paramètres sauvegardés avec succès.',
+	'ACP_WELCOMEROBOT_ERROR'						=> 'Le nom d’utilisateur attribué au robot est invalide.',
+	'ACP_WELCOMEROBOT_ERROR2'						=> 'Sélectionner un forum.',
+));
